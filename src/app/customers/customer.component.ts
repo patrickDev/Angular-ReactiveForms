aa20a50ee32348a302b353a7f4d1ce2b2c1fb73e
Patrick Ngueguim <patrick.ngueguim@gm.com>
2018-11-15 16:11:03 -0600

    Using SetValue to set all value on the form  and PatchValue to subset(few) value on the form

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -22,6 +22,14 @@ export class CustomerComponent implements OnInit {
     });
   }
 
+  populateTestData(): void{
+    this.customerForm.patchValue({  //use setValue to set all the value in the form control and PatchValue to set a value to subset of value on the form                         
+      firstName: 'Patrick', 
+      lastName: 'Herve', 
+    
+      sendCatalog: false
+    });
+  }
   save() {
    console.log(this.customerForm);
    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
