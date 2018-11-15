08a03d4baf2f0ed0961df8a1127d1567c4bedce9
Patrick Ngueguim <patrick.ngueguim@gm.com>
2018-11-15 16:27:56 -0600

    Use FormBuilder compoment

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup,  FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
@@ -11,15 +11,18 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;  
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+
+  }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-        firstName: new FormControl(), 
-        lastName: new FormControl(), 
-        email:  new FormControl(), 
-        sendCatalog: new FormControl(true) //true is defauld value
-    });
+    this.customerForm = this.fb.group({
+      firstName: '', 
+      lastName: '', 
+      email:  '', 
+      sendCatalog: true
+    })
+    
   }
 
   populateTestData(): void{
